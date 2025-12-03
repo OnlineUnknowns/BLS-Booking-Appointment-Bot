aa674d1e092f45bfdaf4e756304204aca7be8f4b
Vanish Now <advinistrator@gmail.com>
2025-12-03 22:45:43 +0200

    Update Data.py

--- Data.py
+++ Data.py
@@ -1,7 +1,7 @@
 # data.py
 # Simple configuration and filenames
 
-CONTACT_PHONE = "+201029107547"
+CONTACT_PHONE = "+201286016083"
 
 # filenames used by the system (stored in the same folder)
 SAMPLE_SLOTS_FILE = "slots.json"
